63aed2243ceb1dbe9b3356fde9694b3bf545d4d0
Roman <kinash0226@gmail.com>
2020-11-08 00:00:09 +0200

    second test

---
+++ 5.2/Unit 5.2/Unit 5.2.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Unit52
+{
+	TEST_CLASS(Unit52)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
